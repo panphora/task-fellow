dc2583f91c9c7394742c8578020a36699c0c206f
David Miranda <david@storylog.com>
2019-06-27 16:08:04 -0400

    init code working better, fixed bugs with sortable

--- public/index.js
+++ public/index.js
@@ -1,15 +1,49 @@
-import { callSaveFunction } from './js/remake-init';
 import './js/user-cookies';
+import { init, getDataFromRootNode, callSaveFunction } from 'remakejs/dist/bundle.es6';
 import Sortable from 'sortablejs';
 
+// init remake
+init({
+  addItemCallback: function ({itemElem, templateName}) {
+    let stackElem = itemElem;
+    
+    if (templateName === "stack") {
+      var sortable = Sortable.create(stackElem.querySelector(".tacks"), {
+        group: "tacks",
+        onEnd: function (event) {
+          callSaveFunction({targetElement: stackElem})
+        }
+      });
+    }
+  }
+});
+
+
+// init sortable
 
 var tackLists = document.querySelectorAll('.tacks');
 
-Array.from(tackLists).forEach(el => {
-  var sortable = Sortable.create(el, {
+Array.from(tackLists).forEach(tacksListElem => {
+  var sortable = Sortable.create(tacksListElem, {
     group: "tacks",
     onEnd: function (event) {
-      callSaveFunction({targetElement: el})
+      callSaveFunction({targetElement: tacksListElem})
     }
   });
 });
+
+
+window.getDataFromRootNode = getDataFromRootNode;
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public/js/remake-init.js
+++
@@ -1,7 +0,0 @@
-import { init, getDataFromRootNode, callSaveFunction } from 'remakejs/dist/bundle.es6';
-
-init();
-
-export {
-  callSaveFunction
-}
