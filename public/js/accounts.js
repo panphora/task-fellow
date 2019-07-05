c39b167ee905a1ffe277524f40a5f49bc3f2b1c2
David Miranda <david@storylog.com>
2019-07-04 20:59:42 -0400

    better styling on sign up page

--- public/js/accounts.js
+++ public/js/accounts.js
@@ -1,15 +1,15 @@
 let formElem = document.querySelector("[action='/sign-up']");
 
-formElem.addEventListener("submit", function (event) {
-  let passwordInputElem = event.currentTarget.querySelector("[type='password']");
-  let password = passwordInputElem.value;
+if (formElem) {
+  formElem.addEventListener("submit", function (event) {
+    let passwordInputElem = event.currentTarget.querySelector("[type='password']");
+    let password = passwordInputElem.value;
 
-  // at least 6 characters, one symbol, one upper case, one number
-  let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[- !"#\$%&'\(\)\*\+,\./:;<=>\?@\[\\\]\^_`{\|}~])(?=.{8,})/;
-  let isValidPassword = passwordRegex.test(password);
+    let isValidPassword = password.length > 7;
 
-  if (!isValidPassword) {
-    event.preventDefault();
-    event.currentTarget.classList.add("has-invalid-password");
-  }
-});
+    if (!isValidPassword) {
+      event.preventDefault();
+      event.currentTarget.classList.add("has-invalid-password");
+    }
+  });
+}
